9c1dbbb477f67ff3f7a1b89dc53dafd155648f3a
Márcio Duarte <maethorin@gmail.com>
2014-10-17 18:39:42 -0300

    Retirando descrição completa e versionando

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-bcash',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/lojaintegrada/LI-Pagador-Bcash',
     license='MIT',
     description='Meio de pagamento usando o Bcash (http://www.bcash.com.br/)',

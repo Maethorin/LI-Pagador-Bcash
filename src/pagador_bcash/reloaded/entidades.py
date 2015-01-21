9e24c2cab1706b0eabff26cc08d8b9006974c6e7
Márcio Duarte <maethorin@gmail.com>
2015-01-21 18:01:22 -0200

    Criando malote com sucesso

--- src/pagador_bcash/reloaded/entidades.py
+++ src/pagador_bcash/reloaded/entidades.py
@@ -13,97 +13,70 @@ class Entrega(entidades.Entrega):
 class Malote(entidades.Malote):
     def __init__(self):
         super(Malote, self).__init__()
-        self.id_plataforma = None
-        self.tipo_integracao = None
-        self.email_loja = None
-        self.email = None
-        self.url_retorno = None
-        self.redirect = None
-        self.redirect_time = None
-        self.id_pedido = None
-        self.frete = None
-        self.tipo_frete = None
-        self.cpf = None
-        self.rg = None
-        self.cliente_razao_social = None
-        self.cliente_cnpj = None
-        self.data_nascimento = None
-        self.sexo = None
-        self.nome = None
-        self.telefone = None
-        self.celular = None
-        self.cep = None
-        self.endereco = None
-        self.complemento = None
-        self.bairro = None
-        self.cidade = None
-        self.estado = None
-        self.desconto = None
-        self.hash = None
 
-    def cria_item_venda(self, indice, codigo, descricao, qtde, valor):
+    def _cria_item_venda(self, retorno, indice, codigo, descricao, qtde, valor):
         indice += 1
-        setattr(self, "produto_codigo_{}".format(indice), codigo)
-        setattr(self, "produto_descricao_{}".format(indice), descricao)
-        setattr(self, "produto_qtde_{}".format(indice), qtde)
-        setattr(self, "produto_valor_{}".format(indice), valor)
+        retorno['produto_codigo_{}'.format(indice)] = codigo
+        retorno['produto_descricao_{}'.format(indice)] = descricao
+        retorno['produto_qtde_{}'.format(indice)] = qtde
+        retorno['produto_valor_{}'.format(indice)] = valor
 
-    def _gerar_hash(self, valores_formulario):
+    def _gerar_hash(self, valores_formulario, configuracao):
         em_ordem = [(k, valores_formulario[k]) for k in sorted(valores_formulario.keys())]
         codificado = []
         for k, v in em_ordem:
             if isinstance(v, unicode):
                 v = v.encode('utf-8')
-            if isinstance(k, unicode):
-                k = k.encode('utf-8')
             codificado.append((k, v))
-        if self.configuracao_pagamento.token:
-            token = self.configuracao_pagamento.token.strip()
+        if configuracao['token']:
+            token = configuracao['token'].strip()
         else:
             token = ''
-        tudo = "%s%s" % (urlencode(codificado), token)
+        tudo = '{}{}'.format(urlencode(codificado), token)
         return md5(tudo).hexdigest()
 
-    def monta_conteudo(self, pedido, dados_complementares):
-        # parametros = ParametrosBcash("bcash", id=self.pedido.conta_id)
-        self.id_plataforma = 'parametros.id_plataforma'
-        self.tipo_integracao = 'PAD',
-        self.email_loja = self.configuracao_pagamento.usuario,
-        self.id_pedido = pedido.numero,
-        self.email = pedido.cliente.email,
-        self.url_retorno = '{}/success/?next_url={}&referencia={}'.format(settings.BCASH_NOTIFICATION_URL.format(pedido.conta_id), dados_complementares["next_url"], pedido.numero),
-        self.redirect = 'true',
-        self.redirect_time = 30,
-        self.frete = self.formatador.formata_decimal(self.valor_envio),
-        self.tipo_frete = pedido.pedido_envio.envio.nome,
-        self.nome = self.formatador.trata_unicode_com_limite((pedido.endereco_entrega.nome or pedido.cliente.email)),
-        self.telefone = pedido.telefone_principal,
-        self.celular = pedido.telefone_celular,
-        self.cep = pedido.endereco_entrega.cep,
-        self.endereco = u'{}, {}'.format(pedido.endereco_entrega.endereco, pedido.endereco_entrega.numero),
-        self.complemento = self.formatador.trata_unicode_com_limite(pedido.endereco_entrega.complemento),
-        self.bairro = self.formatador.trata_unicode_com_limite(pedido.endereco_entrega.bairro),
-        self.cidade = self.formatador.trata_unicode_com_limite(pedido.endereco_entrega.cidade),
-        self.estado = pedido.endereco_entrega.estado,
-        self.desconto = self.formatador.formata_decimal(self.valor_desconto)
+    def monta_conteudo(self, pedido, configuracao=None, dados_complementares=None):
+        retorno = {
+            'id_plataforma': dados_complementares['id_plataforma'],
+            'tipo_integracao': 'PAD',
+            'email_loja': configuracao['usuario'],
+            'id_pedido': pedido.numero,
+            'email': pedido.cliente['email'],
+            'url_retorno': '{}/success/?next_url={}&referencia={}'.format(dados_complementares["url_retorno"], dados_complementares["next_url"], pedido.numero),
+            'redirect': 'true',
+            'redirect_time': 30,
+            'frete': self.formatador.formata_decimal(self.valor_envio(pedido)),
+            'tipo_frete': pedido.forma_envio,
+            'nome': self.formatador.trata_unicode_com_limite((pedido.endereco_entrega['nome'] or pedido.cliente['email'])),
+            'telefone': pedido.telefone_principal,
+            'celular': pedido.telefone_celular,
+            'cep': pedido.endereco_entrega['cep'],
+            'endereco': u'{}, {}'.format(pedido.endereco_entrega['endereco'], pedido.endereco_entrega['numero']),
+            'complemento': self.formatador.trata_unicode_com_limite(pedido.endereco_entrega['complemento']),
+            'bairro': self.formatador.trata_unicode_com_limite(pedido.endereco_entrega['bairro']),
+            'cidade': self.formatador.trata_unicode_com_limite(pedido.endereco_entrega['cidade']),
+            'estado': pedido.endereco_entrega['estado'],
+            'desconto': self.formatador.formata_decimal(self.valor_desconto(pedido)),
+        }
+        if pedido.endereco_entrega['tipo'] == 'PF':
+            retorno['cpf'] = pedido.endereco_entrega['cpf']
+            retorno['rg'] = pedido.endereco_entrega['rg']
+            retorno['sexo'] = pedido.cliente['sexo']
+            if pedido.cliente['data_nascimento']:
+                retorno['data_nascimento'] = pedido.cliente['data_nascimento'].strftime('%d/%m/%Y')
+        if pedido.endereco_entrega['tipo'] == 'PJ':
+            retorno['cliente_razao_social'] = self.formatador.trata_unicode_com_limite(pedido.endereco_entrega['razao_social'])
+            retorno['cliente_cnpj'] = pedido.endereco_entrega['cnpj']
 
-        if pedido.endereco_entrega.tipo == 'PF':
-            self.cpf = pedido.endereco_entrega.cpf
-            self.rg = pedido.endereco_entrega.rg
-            self.sexo = pedido.cliente.sexo
-            if pedido.cliente.data_nascimento:
-                self.data_nascimento = '{}/{}/{}'.format(pedido.cliente.data_nascimento.day, pedido.cliente.data_nascimento.month, pedido.cliente.data_nascimento.year)
-        elif pedido.endereco_entrega.tipo == 'PJ':
-            self.cliente_razao_social = self.formatador.trata_unicode_com_limite(pedido.endereco_entrega.razao_social)
-            self.cliente_cnpj = pedido.endereco_entrega.cnpj
-
-        for indice, item in enumerate(pedido.itens.all()):
-            self.cria_item_venda(
+        for indice, item in enumerate(pedido.items):
+            self._cria_item_venda(
+                retorno,
                 indice,
-                codigo=self.formatador.trata_unicode_com_limite(item.sku, 100),
-                descricao=self.formatador.trata_unicode_com_limite(item.nome, 255),
-                qtde=self.formatador.formata_decimal(item.quantidade, como_int=True),
-                valor=self.formatador.formata_decimal(item.preco_venda)
+                codigo=self.formatador.trata_unicode_com_limite(item['sku'], 100),
+                descricao=self.formatador.trata_unicode_com_limite(item['nome'], 255),
+                qtde=self.formatador.formata_decimal(item['quantidade'], como_int=True),
+                valor=self.formatador.formata_decimal(item['preco_venda'])
             )
 
-        self.hash = self._gerar_hash(self.to_dict())
+        retorno['hash'] = self._gerar_hash(retorno, configuracao)
+        return retorno
---
+++ tests/unitarios/test_entidades.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+import unittest
+from datetime import datetime
+from decimal import Decimal
+from mock import MagicMock, patch
+
+from pagador_bcash.reloaded import entidades
+from pagador.reloaded import entidades as pagador_entidades
+
+
+class GerandoMalote(unittest.TestCase):
+    def _cria_item(self, indice):
+        item = MagicMock()
+        item.sku = 'SKU_{}'.format(indice)
+        item.nome = 'Nome Item {}'.format(indice)
+        item.quantidade = Decimal('1.00')
+        item.preco_venda = Decimal('12.30')
+        return item
+
+    def test_gera_hash_sem_unicode(self):
+        valores_formulario = {'chave2': 'valor2', 'chave1': 1}
+        configuracao = {'token': 'TOKEN'}
+        pagador_entidades.Pedido._repositorio = MagicMock()
+        with patch('pagador_bcash.reloaded.entidades.md5') as md5_mock:
+            hexdig = md5_mock.return_value
+            hexdig.hexdigest.return_value = 'HASH'
+            malote = entidades.Malote()
+            malote._gerar_hash(valores_formulario, configuracao).should.be.equal('HASH')
+            md5_mock.assert_called_with('chave1=1&chave2=valor2TOKEN')
+
+    def test_gera_hash_com_especial(self):
+        valores_formulario = {'chave2': 'colocação', 'chave1': 1}
+        configuracao = {'token': 'TOKEN'}
+        pagador_entidades.Pedido._repositorio = MagicMock()
+        with patch('pagador_bcash.reloaded.entidades.md5') as md5_mock:
+            hexdig = md5_mock.return_value
+            hexdig.hexdigest.return_value = 'HASH'
+            malote = entidades.Malote()
+            malote._gerar_hash(valores_formulario, configuracao).should.be.equal('HASH')
+            md5_mock.assert_called_with('chave1=1&chave2=coloca%C3%A7%C3%A3oTOKEN')
+
+    def test_gera_hash_com_unicode(self):
+        valores_formulario = {'chave2': u'colocação', 'chave1': 1}
+        configuracao = {'token': 'TOKEN'}
+        pagador_entidades.Pedido._repositorio = MagicMock()
+        with patch('pagador_bcash.reloaded.entidades.md5') as md5_mock:
+            hexdig = md5_mock.return_value
+            hexdig.hexdigest.return_value = 'HASH'
+            malote = entidades.Malote()
+            malote._gerar_hash(valores_formulario, configuracao).should.be.equal('HASH')
+            md5_mock.assert_called_with('chave1=1&chave2=coloca%C3%A7%C3%A3oTOKEN')
+
+    def test_gera_hash_sem_token(self):
+        valores_formulario = {'chave2': u'colocação', 'chave1': 1}
+        configuracao = {'token': None}
+        pagador_entidades.Pedido._repositorio = MagicMock()
+        with patch('pagador_bcash.reloaded.entidades.md5') as md5_mock:
+            hexdig = md5_mock.return_value
+            hexdig.hexdigest.return_value = 'HASH'
+            malote = entidades.Malote()
+            malote._gerar_hash(valores_formulario, configuracao).should.be.equal('HASH')
+            md5_mock.assert_called_with('chave1=1&chave2=coloca%C3%A7%C3%A3o')
+
+    def test_monta_conteudo_com_pessoa_fisica(self):
+        pedido_repositorio = MagicMock()
+        pedido_repositorio.cliente.email = 'cliente@teste.com'
+        pedido_repositorio.cliente.sexo = 'M'
+        pedido_repositorio.cliente.data_nascimento = datetime(1970, 2, 23)
+        pedido_repositorio.valor_envio = Decimal('15.60')
+        pedido_repositorio.valor_desconto = Decimal('10.60')
+        pedido_repositorio.valor_subtotal = Decimal('136.40')
+        pedido_repositorio.pedido_envio.envio.nome = 'ENVIO'
+        pedido_repositorio.endereco_entrega.tipo = 'PF'
+        pedido_repositorio.endereco_entrega.nome = 'Cliente Teste'
+        pedido_repositorio.endereco_entrega.razao_social = None
+        pedido_repositorio.endereco_entrega.cpf = '11122233344'
+        pedido_repositorio.endereco_entrega.cnpj = None
+        pedido_repositorio.endereco_entrega.rg = '11222333-4'
+        pedido_repositorio.endereco_entrega.endereco = 'Rua Teste'
+        pedido_repositorio.endereco_entrega.numero = '33'
+        pedido_repositorio.endereco_entrega.complemento = None
+        pedido_repositorio.endereco_entrega.bairro = 'Bairro'
+        pedido_repositorio.endereco_entrega.cidade = 'Cidade'
+        pedido_repositorio.endereco_entrega.estado = 'TT'
+        pedido_repositorio.endereco_entrega.cep = '22123000'
+        pedido_repositorio.telefone_principal = '2122224444'
+        pedido_repositorio.telefone_celular = '21999987777'
+        pedido_repositorio.items = [self._cria_item(1), self._cria_item(2), self._cria_item(3)]
+        pagador_entidades.Pedido._repositorio = MagicMock()
+        pagador_entidades.Pedido._repositorio.obter_unico.return_value = pedido_repositorio
+        pedido = pagador_entidades.Pedido(23, 234)
+        malote = entidades.Malote()
+        configuracao = {
+            'usuario': 'bcash_user',
+            'token': 'TOKEN'
+        }
+        dados_complementares = {
+            'url_retorno': 'http:\\bcash.url_retorno.com',
+            'id_plataforma': 'id_plataforma',
+            'next_url': 'http:\\urlde.redirect.com'
+        }
+        malote._gerar_hash = MagicMock(return_value='HASH')
+        malote.monta_conteudo(pedido, configuracao, dados_complementares).should.be.equal({
+            'bairro': 'Bairro',
+            'celular': '21999987777',
+            'cep': '22123000',
+            'cidade': 'Cidade',
+            'complemento': '',
+            'cpf': '11122233344',
+            'data_nascimento': '23/02/1970',
+            'desconto': '10.60',
+            'email': 'cliente@teste.com',
+            'email_loja': 'bcash_user',
+            'endereco': u'Rua Teste, 33',
+            'estado': 'TT',
+            'frete': '15.60',
+            'hash': 'HASH',
+            'id_pedido': 23,
+            'id_plataforma': 'id_plataforma',
+            'nome': 'Cliente Teste',
+            'redirect': 'true',
+            'redirect_time': 30,
+            'rg': '11222333-4',
+            'sexo': 'M',
+            'telefone': '2122224444',
+            'tipo_frete': 'ENVIO',
+            'tipo_integracao': 'PAD',
+            'url_retorno': 'http:\\bcash.url_retorno.com/success/?next_url=http:\\urlde.redirect.com&referencia=23',
+            'produto_codigo_1': 'SKU_1',
+            'produto_descricao_1': 'Nome Item 1',
+            'produto_valor_1': '12.30',
+            'produto_qtde_1': 1,
+            'produto_codigo_2': 'SKU_2',
+            'produto_descricao_2': 'Nome Item 2',
+            'produto_qtde_2': 1,
+            'produto_valor_2': '12.30',
+            'produto_codigo_3': 'SKU_3',
+            'produto_descricao_3': 'Nome Item 3',
+            'produto_qtde_3': 1,
+            'produto_valor_3': '12.30',
+        })
+
+    def test_monta_conteudo_com_pessoa_juridica(self):
+        pedido_repositorio = MagicMock()
+        pedido_repositorio.cliente.email = 'cliente@teste.com'
+        pedido_repositorio.cliente.sexo = None
+        pedido_repositorio.cliente.data_nascimento = None
+        pedido_repositorio.valor_envio = Decimal('15.60')
+        pedido_repositorio.valor_desconto = Decimal('10.60')
+        pedido_repositorio.valor_subtotal = Decimal('136.40')
+        pedido_repositorio.pedido_envio.envio.nome = 'ENVIO'
+        pedido_repositorio.endereco_entrega.tipo = 'PJ'
+        pedido_repositorio.endereco_entrega.razao_social = u'Razão Social Teste S/A'
+        pedido_repositorio.endereco_entrega.nome = None
+        pedido_repositorio.endereco_entrega.cpf = None
+        pedido_repositorio.endereco_entrega.cnpj = '12345678901234'
+        pedido_repositorio.endereco_entrega.rg = None
+        pedido_repositorio.endereco_entrega.endereco = 'Rua Teste'
+        pedido_repositorio.endereco_entrega.numero = '33'
+        pedido_repositorio.endereco_entrega.complemento = 'Empresa'
+        pedido_repositorio.endereco_entrega.bairro = 'Bairro'
+        pedido_repositorio.endereco_entrega.cidade = 'Cidade'
+        pedido_repositorio.endereco_entrega.estado = 'TT'
+        pedido_repositorio.endereco_entrega.cep = '22123000'
+        pedido_repositorio.telefone_principal = '2122224444'
+        pedido_repositorio.telefone_celular = '21999987777'
+        pedido_repositorio.items = [self._cria_item(1), self._cria_item(2), self._cria_item(3)]
+        pagador_entidades.Pedido._repositorio = MagicMock()
+        pagador_entidades.Pedido._repositorio.obter_unico.return_value = pedido_repositorio
+        pedido = pagador_entidades.Pedido(23, 234)
+        malote = entidades.Malote()
+        configuracao = {
+            'usuario': 'bcash_user',
+            'token': 'TOKEN'
+        }
+        dados_complementares = {
+            'url_retorno': 'http:\\bcash.url_retorno.com',
+            'id_plataforma': 'id_plataforma',
+            'next_url': 'http:\\urlde.redirect.com'
+        }
+        malote._gerar_hash = MagicMock(return_value='HASH')
+        malote.monta_conteudo(pedido, configuracao, dados_complementares).should.be.equal({
+            'bairro': 'Bairro',
+            'celular': '21999987777',
+            'cep': '22123000',
+            'cidade': 'Cidade',
+            'complemento': 'Empresa',
+            'cliente_cnpj': '12345678901234',
+            'desconto': '10.60',
+            'email': 'cliente@teste.com',
+            'email_loja': 'bcash_user', 'endereco': u'Rua Teste, 33',
+            'estado': 'TT',
+            'frete': '15.60',
+            'hash': 'HASH',
+            'id_pedido': 23,
+            'id_plataforma': 'id_plataforma',
+            'nome': 'cliente@teste.com',
+            'cliente_razao_social': 'Raz\xc3\xa3o Social Teste S/A',
+            'redirect': 'true',
+            'redirect_time': 30,
+            'telefone': '2122224444',
+            'tipo_frete': 'ENVIO',
+            'tipo_integracao': 'PAD',
+            'url_retorno': 'http:\\bcash.url_retorno.com/success/?next_url=http:\\urlde.redirect.com&referencia=23',
+            'produto_codigo_1': 'SKU_1',
+            'produto_descricao_1': 'Nome Item 1',
+            'produto_valor_1': '12.30',
+            'produto_qtde_1': 1,
+            'produto_codigo_2': 'SKU_2',
+            'produto_descricao_2': 'Nome Item 2',
+            'produto_qtde_2': 1,
+            'produto_valor_2': '12.30',
+            'produto_codigo_3': 'SKU_3',
+            'produto_descricao_3': 'Nome Item 3',
+            'produto_qtde_3': 1,
+            'produto_valor_3': '12.30',
+        })

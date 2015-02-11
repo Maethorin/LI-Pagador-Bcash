a36d4bbbcf7971d7b803789b87fc89d7b9e27f6b
maethorin <maethorin@gmail.com>
2015-02-11 20:05:57 -0200

    Criando formulario

--- src/pagador_bcash/reloaded/entidades.py
+++ src/pagador_bcash/reloaded/entidades.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from urllib import urlencode
 from hashlib import md5
+from li_common.padroes import cadastro
 
 from pagador.reloaded import entidades
 
@@ -75,3 +76,25 @@ class Malote(entidades.Malote):
 
         retorno['hash'] = self._gerar_hash(retorno, configuracao)
         return retorno
+
+
+class Formulario(cadastro.Formulario):
+    PARCELAS = [(x, x) for x in range(1, 24)]
+    PARCELAS.insert(0, (24, "Todas"))
+    usuario = cadastro.CampoFormulario("usuario", "Seu email no Bcash", requerido=True, tamanho_max=128, ordem=1)
+    token = cadastro.CampoFormulario("token", "Sua chave acesso", requerido=True, tamanho_max=128, ordem=2)
+    valor_minimo_aceitado = cadastro.CampoFormulario("valor_minimo_aceitado", u"Valor mínimo", requerido=False, decimais=2, ordem=3, tipo=cadastro.TipoDeCampo.decimal, texto_ajuda=u"Informe o valor mínimo para exibir esta forma de pagamento.")
+    valor_minimo_parcela = cadastro.CampoFormulario("valor_minimo_parcela", u"Valor mínimo da parcela", requerido=False, decimais=2, ordem=4, tipo=cadastro.TipoDeCampo.decimal)
+    mostrar_parcelamento = cadastro.CampoFormulario("mostrar_parcelamento", "Marque para mostrar o parcelamento na listagem e na página do produto.", tipo=cadastro.TipoDeCampo.boleano, requerido=False, ordem=5)
+    maximo_parcelas = cadastro.CampoFormulario("maximo_parcelas", "Máximo de parcelas", tipo=cadastro.TipoDeCampo.escolha, requerido=False, ordem=6, texto_ajuda=u"Quantidade máxima de parcelas para esta forma de pagamento.", opcoes=PARCELAS)
+    parcelas_sem_juros = cadastro.CampoFormulario("parcelas_sem_juros", "Parcelas sem juros", tipo=cadastro.TipoDeCampo.escolha, requerido=False, ordem=7, texto_ajuda=u"Número de parcelas sem juros para esta forma de pagamento.", opcoes=PARCELAS)
+
+
+class ConfiguracaoMeioPagamento(entidades.ConfiguracaoMeioPagamento):
+    _campos = ['ativo', 'usuario', 'token', 'valor_minimo_aceitado', 'valor_minimo_parcela', 'mostrar_parcelamento', 'maximo_parcelas', 'parcelas_sem_juros']
+    _codigo_gateway = 2
+
+    def __init__(self, loja_id):
+        super(ConfiguracaoMeioPagamento, self).__init__(loja_id)
+        self.preencher_do_gateway(self._codigo_gateway, self._campos)
+        self.formulario = Formulario().to_dict()

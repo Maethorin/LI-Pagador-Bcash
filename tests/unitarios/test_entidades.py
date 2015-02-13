51869b2687117b45d2367ef33c99561e3bd2df43
maethorin <maethorin@gmail.com>
2015-02-13 18:27:18 -0200

    Refatorando testes

--- tests/unitarios/test_entidades.py
+++ tests/unitarios/test_entidades.py
@@ -218,20 +218,22 @@ class GerandoMalote(unittest.TestCase):
         })
 
 
-CAMPOS = ['ativo', 'usuario', 'token', 'valor_minimo_aceitado', 'valor_minimo_parcela', 'mostrar_parcelamento', 'maximo_parcelas', 'parcelas_sem_juros']
-
-
 class ConfiguracaoMeioPagamento(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(ConfiguracaoMeioPagamento, self).__init__(*args, **kwargs)
+        self.campos = ['ativo', 'usuario', 'token', 'valor_minimo_aceitado', 'valor_minimo_parcela', 'mostrar_parcelamento', 'maximo_parcelas', 'parcelas_sem_juros']
+        self.codigo_gateway = 2
+
     def test_deve_ter_os_campos_especificos_na_classe(self):
-        entidades.ConfiguracaoMeioPagamento._campos.should.be.equal(CAMPOS)
+        entidades.ConfiguracaoMeioPagamento._campos.should.be.equal(self.campos)
 
     def test_deve_ter_codigo_gateway(self):
-        entidades.ConfiguracaoMeioPagamento._codigo_gateway.should.be.equal(2)
+        entidades.ConfiguracaoMeioPagamento._codigo_gateway.should.be.equal(self.codigo_gateway)
 
     @mock.patch('pagador_bcash.reloaded.entidades.ConfiguracaoMeioPagamento.preencher_do_gateway', autospec=True)
     def test_deve_preencher_do_gateway_na_inicializacao(self, preencher_mock):
         configuracao = entidades.ConfiguracaoMeioPagamento(234)
-        preencher_mock.assert_called_with(configuracao, 2, CAMPOS)
+        preencher_mock.assert_called_with(configuracao, self.codigo_gateway, self.campos)
 
     @mock.patch('pagador_bcash.reloaded.entidades.ConfiguracaoMeioPagamento.preencher_do_gateway', autospec=True)
     def test_deve_definir_formulario_na_inicializacao(self, preencher_mock):

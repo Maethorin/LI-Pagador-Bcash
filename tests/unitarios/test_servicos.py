fd2a8629baab9471d78072d959b1dcee036d29ab
maethorin <maethorin@gmail.com>
2015-03-01 14:06:57 -0300

    Corrigindo testes

--- tests/unitarios/test_servicos.py
+++ tests/unitarios/test_servicos.py
@@ -5,19 +5,13 @@ from pagador_bcash.reloaded import servicos
 
 
 class BcashEntregandoPagamento(unittest.TestCase):
-    def test_resultado_no_padrao(self):
-        resultado = servicos.Resultado({'chave': 'valor'})
-        resultado.sucesso.should.be.truthy
-        resultado.conteudo.should.be.equal({'dados': {'chave': 'valor'}})
-
     def test_entrega_tem_malote(self):
         entrega = servicos.EntregaPagamento(234)
         entrega.tem_malote.should.be.truthy
 
-    def test_envia_pagamento(self):
+    def test_processa_pagamento(self):
         entrega = servicos.EntregaPagamento(234)
         entrega.malote = mock.MagicMock()
         entrega.malote.to_dict.return_value = 'dados'
-        entrega.enviar_pagamento()
-        entrega.resultado.sucesso.should.be.truthy
-        entrega.resultado.conteudo.should.be.equal({'dados': 'dados'})
+        entrega.processa_dados_de_pagamento()
+        entrega.resultado.should.be.equal({'dados': 'dados'})

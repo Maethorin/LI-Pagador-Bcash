01dccd5a1301c19f391fa77a4027afc6e530a928
maethorin <maethorin@gmail.com>
2015-03-13 11:43:51 -0300

    Testes Integração

--- tests/integracao/test_acesso_via_api.py
+++ tests/integracao/test_acesso_via_api.py
@@ -32,3 +32,39 @@ class BcashConfiguracaoMeioDePagamentoDaLoja(TestBase):
         json.loads(response.data).should.be.equal({u'metadados': {u'api': u'API Pagador', u'resultado': u'sucesso', u'versao': u'1.0'}, u'sucesso': {u'configuracao_pagamento': u'BCASH'}})
         response.status_code.should.be.equal(200)
         configuracao.salvar_formulario.assert_called_with({'token': u'ZES'})
+
+
+class BcashEnviandoPagamento(TestBase):
+    url = '/loja/8/meio-pagamento/pagamento_digital/enviar/1234/1'
+
+    @mock.patch('pagador.servicos.GerenciaPedido', mock.MagicMock())
+    @mock.patch('pagador.servicos.GravaEvidencia', mock.MagicMock())
+    @mock.patch('pagador_bcash.servicos.EntregaPagamento')
+    def test_deve_enviar_pagamento(self, entrega_mock):
+        entrega_mock.return_value = mock.MagicMock(redirect_para=None, resultado={'zas': 'pagamento-enviado'})
+        response = self.app.post(self.url, follow_redirects=True, headers={'authorization': 'chave_aplicacao CHAVE-TESTE'})
+        json.loads(response.data).should.be.equal({u'metadados': {u'api': u'API Pagador', u'resultado': u'sucesso', u'versao': u'1.0'}, u'sucesso': {u'zas': u'pagamento-enviado'}})
+
+
+class BcashRegistrandoResultado(TestBase):
+    url = '/meio-pagamento/pagamento_digital/retorno/8/resultado'
+
+    @mock.patch('pagador.servicos.GerenciaPedido', mock.MagicMock())
+    @mock.patch('pagador.servicos.GravaEvidencia', mock.MagicMock())
+    @mock.patch('pagador_bcash.servicos.RegistraResultado')
+    def test_deve_enviar_pagamento(self, registra_mock):
+        registra_mock.return_value = mock.MagicMock(redirect_para=None, resultado={'zas': 'resultado-registrado'})
+        response = self.app.post(self.url, follow_redirects=True, headers={'authorization': 'chave_aplicacao CHAVE-TESTE'})
+        json.loads(response.data).should.be.equal({u'metadados': {u'api': u'API Pagador', u'resultado': u'sucesso', u'versao': u'1.0'}, u'sucesso': {u'zas': u'resultado-registrado'}})
+
+
+class BcashRegistrandoNotificacao(TestBase):
+    url = '/meio-pagamento/pagamento_digital/retorno/8/notificacao'
+
+    @mock.patch('pagador.servicos.GerenciaPedido', mock.MagicMock())
+    @mock.patch('pagador.servicos.GravaEvidencia', mock.MagicMock())
+    @mock.patch('pagador_bcash.servicos.RegistraNotificacao')
+    def test_deve_enviar_pagamento(self, registra_mock):
+        registra_mock.return_value = mock.MagicMock(redirect_para=None, resultado={'zas': 'notificacao-registrado'})
+        response = self.app.post(self.url, follow_redirects=True, headers={'authorization': 'chave_aplicacao CHAVE-TESTE'})
+        json.loads(response.data).should.be.equal({u'metadados': {u'api': u'API Pagador', u'resultado': u'sucesso', u'versao': u'1.0'}, u'sucesso': {u'zas': u'notificacao-registrado'}})

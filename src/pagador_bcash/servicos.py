2c5d8893763bba072f9a00071f8f4b4adc35071d
maethorin <maethorin@gmail.com>
2015-03-21 21:42:06 -0300

    Corrigindo erro de id_pedido

--- src/pagador_bcash/servicos.py
+++ src/pagador_bcash/servicos.py
@@ -15,11 +15,8 @@ class EntregaPagamento(servicos.EntregaPagamento):
 
 class SituacoesDePagamento(servicos.SituacoesDePagamento):
     DE_PARA = {
-        'Em Andamento': servicos.SituacaoPedido.SITUACAO_AGUARDANDO_PAGTO,
-        'Aprovada': servicos.SituacaoPedido.SITUACAO_PEDIDO_PAGO,
         'Disputa': servicos.SituacaoPedido.SITUACAO_PAGTO_EM_DISPUTA,
         'Devolvida': servicos.SituacaoPedido.SITUACAO_PAGTO_DEVOLVIDO,
-        'Cancelada': servicos.SituacaoPedido.SITUACAO_PEDIDO_CANCELADO,
         'Chargeback': servicos.SituacaoPedido.SITUACAO_PAGTO_CHARGEBACK,
         '0': servicos.SituacaoPedido.SITUACAO_AGUARDANDO_PAGTO,
         '1': servicos.SituacaoPedido.SITUACAO_PEDIDO_PAGO,
@@ -33,11 +30,14 @@ class RegistraResultado(servicos.RegistraResultado):
         self.redirect_para = dados.get('next_url', None)
 
     def monta_dados_pagamento(self):
-        self.pedido_numero = self.dados["referencia"]
-        self.dados_pagamento['identificador_id'] = self.dados['id_transacao']
-        self.dados_pagamento['transacao_id'] = self.dados['id_transacao']
-        self.situacao_pedido = SituacoesDePagamento.do_tipo(self.dados['cod_status'])
-        self.resultado = {'resultado': 'OK'}
+        if 'id_transacao' in self.dados:
+            self.pedido_numero = self.dados["referencia"]
+            self.dados_pagamento['identificador_id'] = self.dados['id_transacao']
+            self.dados_pagamento['transacao_id'] = self.dados['id_transacao']
+            self.situacao_pedido = SituacoesDePagamento.do_tipo(self.dados['cod_status'])
+            self.resultado = 'sucesso'
+        else:
+            self.resultado = 'pendente'
 
 
 class RegistraNotificacao(servicos.RegistraResultado):
@@ -45,8 +45,8 @@ class RegistraNotificacao(servicos.RegistraResultado):
         super(RegistraNotificacao, self).__init__(loja_id, dados)
 
     def monta_dados_pagamento(self):
-        self.pedido_numero = self.dados["pedido"]
+        self.pedido_numero = self.dados["id_pedido"]
         self.dados_pagamento['identificador_id'] = self.dados['id_transacao']
         self.dados_pagamento['transacao_id'] = self.dados['id_transacao']
-        self.situacao_pedido = SituacoesDePagamento.do_tipo(self.dados['status'])
+        self.situacao_pedido = SituacoesDePagamento.do_tipo(self.dados['cod_status'])
         self.resultado = {'resultado': 'OK'}
